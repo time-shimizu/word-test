a0f258013ed4964ebbfed5869fc1000c0d4424a9
taimushimizu <213.f.r48132@gmail.com>
2020-05-19 06:02:58 +0900

    ユーザー登録機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録に成功しました"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {maximum: 20}
-  validates :password, presence: true, length: {minimum: 4}
+  validates :name, presence: true, length: {maximum: 20}, uniqueness: true
+  validates :password, presence: true, length: {minimum: 4}, allow_nil: true
   has_secure_password
 end
