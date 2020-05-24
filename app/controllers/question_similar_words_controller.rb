c6f42208758ef7292aa233e0018914d72a2ab57a
taimushimizu <213.f.r48132@gmail.com>
2020-05-24 21:24:02 +0900

    QuestionSimilarWordsコントローラーが不要になったので削除

--- app/controllers/question_similar_words_controller.rb
+++
@@ -1,8 +0,0 @@
-class QuestionSimilarWordsController < ApplicationController
-  before_action :logged_in_user
-  def destroy
-    QuestionSimilarWord.find(params[:id]).destroy
-    flash[:success] = "類似単語を削除しました"
-    redirect_to questions_path
-  end
-end
