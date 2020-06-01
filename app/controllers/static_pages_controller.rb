f2dfb5ebdcedf8d1eee0fd815cef683389dbe26c
asnica001 <asnica001@asnica001noMacBook-puro.local>
2020-06-01 19:48:16 +0900

    会社のパソコンへの環境移動

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    session[:correct] = 0
+    session[:incorrect] = 0
     rand_number = Question.all.map(&:id).sample
     @question = Question.find(rand_number)
   end
