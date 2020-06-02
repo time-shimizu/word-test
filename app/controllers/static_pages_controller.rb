0217f3fcaceac1f77465e13d7118d6abe681667a
shimizutaimu <shimizu@asnica.co.jp>
2020-06-02 11:36:04 +0900

    単語が登録されてない時にメインページで起きるエラーの解消

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    rand_number = Question.all.map(&:id).sample
-    @question = Question.find(rand_number)
+    unless Question.count == 0
+      rand_number = Question.all.map(&:id).sample
+      @question = Question.find(rand_number)
+    end
   end
 end
